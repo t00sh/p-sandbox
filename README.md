537c08e8608cf9fa09d4242fda5c094d0b60f5f3
-TOSH- <tosh@t0x0sh.org>
2015-09-05 00:04:29 +0200

    p-sandbox ; first commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # p-sandbox
 A ptrace based sandbox. (PoC)
+
+--------------------------------
+
+
+This is a small sandbox to limit the number of allowed syscalls for a process.
+
+There is no configuration file, you must edit the source itself...
+
+
+## To compile :
+
+$ gcc -o p-sandbox p-sandbox.c
+
+
+## To run :
+
+$ ./p-sandbox /sandboxed/process
+
+
+## Author
+
+-TOSH-
---
+++ p-sandbox.c
@@ -0,0 +1,131 @@
+#include <sys/ptrace.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <err.h>
+#include <sys/user.h>
+#include <asm/ptrace.h>
+#include <sys/wait.h>
+#include <asm/unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <sys/types.h>
+
+struct sandbox {
+  pid_t child;
+  const char *progname;
+};
+
+struct sandb_syscall {
+  int syscall;
+  void (*callback)(struct sandbox*, struct user_regs_struct *regs);
+};
+
+struct sandb_syscall sandb_syscalls[] = {
+  {__NR_read,            NULL},
+  {__NR_write,           NULL},
+  {__NR_exit,            NULL},
+  {__NR_brk,             NULL},
+  {__NR_mmap,            NULL},
+  {__NR_access,          NULL},
+  {__NR_open,            NULL},
+  {__NR_fstat,           NULL},
+  {__NR_close,           NULL},
+  {__NR_mprotect,        NULL},
+  {__NR_munmap,          NULL},
+  {__NR_arch_prctl,      NULL},
+  {__NR_exit_group,      NULL},
+  {__NR_getdents,        NULL},
+};
+
+void sandb_kill(struct sandbox *sandb) {
+  kill(sandb->child, SIGKILL);
+  wait(NULL);
+  exit(EXIT_FAILURE);
+}
+
+void sandb_handle_syscall(struct sandbox *sandb) {
+  int i;
+  struct user_regs_struct regs;
+
+  if(ptrace(PTRACE_GETREGS, sandb->child, NULL, &regs) < 0)
+    err(EXIT_FAILURE, "[SANDBOX] Failed to PTRACE_GETREGS:");
+
+  for(i = 0; i < sizeof(sandb_syscalls)/sizeof(*sandb_syscalls); i++) {
+    if(regs.orig_rax == sandb_syscalls[i].syscall) {
+      if(sandb_syscalls[i].callback != NULL)
+        sandb_syscalls[i].callback(sandb, &regs);
+      return;
+    }
+  }
+
+  if(regs.orig_rax == -1) {
+    printf("[SANDBOX] Segfault ?! KILLING !!!\n");
+  } else {
+    printf("[SANDBOX] Trying to use devil syscall (%llu) ?!? KILLING !!!\n", regs.orig_rax);
+  }
+  sandb_kill(sandb);
+}
+
+void sandb_init(struct sandbox *sandb, int argc, char **argv) {
+  pid_t pid;
+
+  pid = fork();
+
+  if(pid == -1)
+    err(EXIT_FAILURE, "[SANDBOX] Error on fork:");
+
+  if(pid == 0) {
+
+    if(ptrace(PTRACE_TRACEME, 0, NULL, NULL) < 0)
+      err(EXIT_FAILURE, "[SANDBOX] Failed to PTRACE_TRACEME:");
+
+    if(execv(argv[0], argv) < 0)
+      err(EXIT_FAILURE, "[SANDBOX] Failed to execv:");
+
+  } else {
+    sandb->child = pid;
+    sandb->progname = argv[0];
+    wait(NULL);
+  }
+}
+
+void sandb_run(struct sandbox *sandb) {
+  int status;
+
+  if(ptrace(PTRACE_SYSCALL, sandb->child, NULL, NULL) < 0) {
+    if(errno == ESRCH) {
+      waitpid(sandb->child, &status, __WALL | WNOHANG);
+      sandb_kill(sandb);
+    } else {
+      err(EXIT_FAILURE, "[SANDBOX] Failed to PTRACE_SYSCALL:");
+    }
+  }
+
+  wait(&status);
+
+  if(WIFEXITED(status))
+    exit(EXIT_SUCCESS);
+
+  if(WIFSTOPPED(status)) {
+    sandb_handle_syscall(sandb);
+  }
+}
+
+int main(int argc, char **argv) {
+  struct sandbox sandb;
+
+  if(argc < 2) {
+    errx(EXIT_FAILURE, "[SANDBOX] Usage : %s <elf> [<arg1...>]", argv[0]);
+  }
+
+  sandb_init(&sandb, argc-1, argv+1);
+
+  for(;;) {
+    sandb_run(&sandb);
+  }
+
+  return EXIT_SUCCESS;
+}
